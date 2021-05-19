dc0bea772b5ea31a6b11bd811a7468d94f96c7ad
Alejo Ramirez Gismondi <ramirezgismondialejo@gmail.com>
2021-05-19 17:20:20 -0300

    deleted react native profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme['color-basic-100'],
+    marginTop: 60
+  },
+}));
 
 const Profile = () => {
+  const themedStyle = useStyles();
+
   return (
-    <div>
-      <h1>PROFILE!</h1>
+    <div className={themedStyle.root}>
+
     </div>
   );
 }
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -15,6 +15,7 @@ const Router = () => {
         <ReverseAuthRoute path={"/login"} component={Login}/>
         <ReverseAuthRoute path={"/register"} component={Register}/>
         <AuthRoute path={'/profile'} component={Profile}/>
+        {/*Change to AuthRoute later*/}
         <Route path={"/"} component={Home}/>
       </Switch>
     </BrowserRouter>
