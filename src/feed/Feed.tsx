5f23e91c7b724f1c5960bed24338ab337e8a36e0
Alejo Ramirez Gismondi <ramirezgismondialejo@gmail.com>
2021-05-21 01:36:18 -0300

    post get request (blocked by CORS)

--- src/feed/Feed.tsx
+++ src/feed/Feed.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Post from "../post/Post";
+import {postUrl} from "../utils/http";
+import axios from "axios";
+import {GettedPost} from "../pages/Home";
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -33,10 +36,13 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+interface Prop {
+  cards: [GettedPost]
+}
 
-const Feed = () => {
+const Feed = (props: Prop) => {
   const classes = useStyles();
+
   const text: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore\n' +
     '          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n' +
     '          consequat.';
@@ -49,8 +55,8 @@ const Feed = () => {
       <main>
         <Container className={classes.cardGrid} maxWidth="md">
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+            {props.cards.map((card) => (
+              <Grid item key={card.id} xs={12} sm={6} md={4}>
                 <Post text={text} date={date} userName={userName}/>
               </Grid>
             ))}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../header/Header";
 import NewPost from "../post/NewPost";
 import Feed from "../feed/Feed";
 import {createStyles, makeStyles, Theme} from "@material-ui/core";
+import axios from "axios";
+import {postUrl} from "../utils/http";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,14 +19,30 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+export interface GettedPost {
+  id: string,
+  text: string,
+  date: string,
+  userName: string
+}
+
 const Home = () => {
   const classes = useStyles();
+
+  const [cards, setCards] = useState<[GettedPost]>();
+
+  useEffect(() => {
+    axios.get(postUrl+`posts`)
+      .then(res => {
+        setCards(res.data)
+      })
+  })
 
   return (
     <div>
       <Header/>
       <NewPost className={classes.newPost}/>
-      <Feed/>
+      <Feed cards={[{id: "0", text: "text", date: "date", userName: "username"}]}/>
     </div>
   );
 }
